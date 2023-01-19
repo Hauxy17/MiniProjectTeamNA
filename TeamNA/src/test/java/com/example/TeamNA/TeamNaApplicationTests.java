db7c8d4d3ae09bc2bce82b6f429e3df95296e8a2
Hauxy17 <ella.costello@yahoo.com>
2023-01-19 13:29:50 +0100

    Initial commit

---
+++ TeamNA/src/test/java/com/example/TeamNA/TeamNaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamNA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamNaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
