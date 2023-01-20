770104a4ff84bae21884331961aae8cb84e476be
Hauxy17 <ella.costello@yahoo.com>
2023-01-20 08:27:48 +0100

    Edited: createUser.html pom.xml(security) User.java UserController.java users.html

--- TeamNA/src/main/java/com/example/TeamNA/UserController.java
+++ TeamNA/src/main/java/com/example/TeamNA/UserController.java
@@ -5,19 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.security.config.http.MatcherType.mvc;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     public String users(Model model) {
-        List<User> users = new ArrayList<>(); // todo get all customers
+        List<User> users = new ArrayList<>();
         model.addAttribute("users", users);
-
         return "users";
+    }
+
+    @GetMapping("/createUser")
+    public String create(Model model, User user) {
+        model.addAttribute("user", user);
+        return "createUser";
     }
 }
