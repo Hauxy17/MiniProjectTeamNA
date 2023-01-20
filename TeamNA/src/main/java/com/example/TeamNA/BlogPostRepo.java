2040d0e70324733bd13f75dcf93c915f2716d8fd
JohnnyLoeve <120627383+JohnnyLoeve@users.noreply.github.com>
2023-01-20 13:11:23 +0100

    Implemented more css for Forum.html

--- TeamNA/src/main/java/com/example/TeamNA/BlogPostRepo.java
+++ TeamNA/src/main/java/com/example/TeamNA/BlogPostRepo.java
@@ -13,4 +13,12 @@ public class BlogPostRepo {
     public List<BlogPost> getBlogPosts() {
         return blogPosts;
     }
+
+    public List<BlogPost> addBlogPost(BlogPost blogPost, String author, String text) {
+        blogPost.setAuthor(author);
+        blogPost.setText(text);
+        blogPosts.add(blogPost);
+        return blogPosts;
+    }
+
 }
