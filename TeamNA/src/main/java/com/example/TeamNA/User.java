5af9b015efbd0e4b59bfbc6a1c0933181d0a3077
line-t-c <line_t_c@hotmail.com>
2023-01-20 11:08:17 +0100

    UserRepo.java edited

--- TeamNA/src/main/java/com/example/TeamNA/User.java
+++ TeamNA/src/main/java/com/example/TeamNA/User.java
@@ -34,6 +34,10 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -41,5 +45,30 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
 
--- TeamNA/src/main/java/com/example/TeamNA/UserRepo.java
+++ TeamNA/src/main/java/com/example/TeamNA/UserRepo.java
@@ -1,75 +1,64 @@
 package com.example.TeamNA;
-
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+
 public class UserRepo {
 
-//    public void deleteUser(String userName) {
-//        User userToDelete = this.getUsername(userName);
-//        if (userToDelete != null) {
-//            users.remove(userToDelete);
-//        }
-//    }
+    private List<User> users;
 
-//    private List<Book> books;
-//
-//    public BookRepository() {
-//        books = new ArrayList<>();
-//        books.add(new Book(1L, "Spring Boot in Action", "Craig Walls", 40));
-//        books.add(new Book(2L, "Pro Spring Boot 2", "Felipe Gutierrez", 24));
-//        books.add(new Book(3L, "Learning Spring Boot 2.0", "Greg L. Turnquist", 22));
-//        books.add(new Book(4L, "Cloud Native Java", "Josh Long and Kenny Bastani", 47));
-//        books.add(new Book(5L, "Spring Boot 2 Recipes", "Marten Deinum", 32));
-//        books.add(new Book(6L, "Mastering Spring Boot 2.0", "Dinesh Rajput", 31));
-//        books.add(new Book(7L, "Spring Boot 2.0 Projects", "Mohamed Shazin Sadakath", 31));
-//        books.add(new Book(8L, "Cloud-Native Applications in Java", "Ajay Mahajan, Munish Kumar Gupta, et al", 49));
-//    }
-//
-//    // get one book
-//    public Book getBook(Long id) {
-//        for (Book book : books) {
-//            if (book.getId().equals(id)) {
-//                return book;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    // get all books
-//    public List<Book> getBooks() {
-//        return books;
-//    }
-//
-//    // add a book
-//    public Book addBook(Book book) {
-//        Book lastBook = books.get(books.size()-1);
-//        book.setId(lastBook.getId()+1); // set an id on the new book, should be unique, will be done by the database in future exercises
-//        books.add(book);
-//        return book;
-//    }
-//
-//    // edit a book
-//    public Book editBook(Book book) {
-//        Book bookToEdit = this.getBook(book.getId());
-//        if (bookToEdit != null) {
-//            bookToEdit.setAuthor(book.getAuthor());
-//            bookToEdit.setTitle(book.getTitle());
-//            bookToEdit.setPrice(book.getPrice());
-//        }
-//        return book;
-//    }
-//
-//    // delete a book
-//    public void deleteBook(Long id) {
-//        Book bookToDelete = this.getBook(id);
-//        if (bookToDelete != null) {
-//            books.remove(bookToDelete);
-//        }
-//    }
+    public UserRepo() {
+        users = new ArrayList<>();
+        users.add(new User("Ella", "AW", "ella@brights.dk", "Hauxy", "1234"));
+        users.add(new User("Line", "AW", "line@brights.dk", "Line", "1234"));
+        users.add(new User("Sion", "AW", "sion@brights.dk", "Johnny Loeve", "1234"));
+    }
+
+    // get one user
+    public User getUser(String userName) {
+        for (User user : users) {
+            if (user.getUsername().equals(userName)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // get all users
+    public List<User> getUsers() {
+        return users;
+    }
+
+    // add a user
+    public User addBook(User user) {
+        User lastBook = users.get(users.size()-1);
+        user.setUsername(lastBook.getUsername()+1); // set an UserName on the new user, should be unique, will be done by the database in future exercises
+        users.add(user);
+        return user;
+    }
+
+    // edit a user
+        public User editUser(User user) {
+        User userToEdit = this.getUser(user.getUsername());
+        if (userToEdit  != null) {
+            userToEdit .setUsername(user.getUsername());
+            userToEdit .setPassword(user.getPassword());
+            userToEdit .setEmail(user.getEmail());
+        }
+        return user;
+    }
+
+    // delete a user
+    public void deleteUser(String userName) {
+        User userToDelete = this.getUser(userName);
+        if (userToDelete != null) {
+            users.remove(userToDelete);
+        }
+    }
+
 
 }
 
