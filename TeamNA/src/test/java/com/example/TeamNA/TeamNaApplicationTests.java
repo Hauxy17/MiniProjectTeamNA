a78265f52d318c98e67f45ee2e25a4629f2d5b7d
line-t-c <line_t_c@hotmail.com>
2023-01-20 13:18:24 +0100

    unitest

--- TeamNA/src/test/java/com/example/TeamNA/TeamNaApplicationTests.java
+++ TeamNA/src/test/java/com/example/TeamNA/TeamNaApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.TeamNA;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TeamNaApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void testUsername() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.get("/")
+		) .andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.content().string(containsString("Username")));
 	}
 
 }
